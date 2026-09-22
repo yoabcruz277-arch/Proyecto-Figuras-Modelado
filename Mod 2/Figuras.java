54db709c322cd4a92a18486129bce8d257d34339
serchoz <serchgamer80@gmail.com>
2026-09-21 22:01:25 -0600

    prueba mod2

---
+++ Mod 2/Figuras.java
@@ -0,0 +1,15 @@
+public class Figuras{
+    public MetricasFigura analiazador(Object figura){
+        //para probar
+        double areaP=100.0;
+        double perimetroP=40.0;
+        int verticesP=4;
+        double circularidadP= 0.777;
+
+
+    
+        //retorna datos inventados
+        return new MetricasFigura(areaP, perimetroP, verticesP, circularidadP);
+
+    }
+}
---
+++ Mod 2/MetricasFigura.java
@@ -0,0 +1,13 @@
+public class MetricasFigura{
+    public double area;
+    public double perimetro;
+    public int vertices;
+    public double circularidad;
+
+    public MetricasFigura(double area, double perimetro, int vertices, double circularidad){
+        this.area=area;
+        this.perimetro=perimetro;
+        this.vertices=vertices;
+        this.circularidad=circularidad;
+    }
+}
