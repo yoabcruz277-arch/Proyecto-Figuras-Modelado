9c940e383824c29d8006a4c123d808dc304fccf6
serchoz <serchgamer80@gmail.com>
2026-09-24 20:45:32 -0600

    actualizao

--- Mod 2/AnalizadorFiguras.java
+++ Mod 2/AnalizadorFiguras.java
@@ -1,4 +1,14 @@
+import java.awt.Point;
+import java.util.List;
+
 public class AnalizadorFiguras{
+
+    private FiltroRDP filtro;
+    public AnalizadorGeometria(){
+        this.filtro=new FiltroRDP();
+    }
+
+
     public MetricasFigura analizador(Object figura){
         //para probar
         double areaP=100.0;
--- Mod 2/FIltroRDP.java
+++ Mod 2/FIltroRDP.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Point;
+
 public class FIltroRDP{
     
+    public List<Point> simplificarContorno(List<Point> puntos, double epsilon){
+        if(epsilon<0){
+            throw new IllegalArgumentException("Epsilon no puede ser menor que cero");
+        }
+        if(puntos==null||puntos.size()<3){
+            return new ArrayList<>(puntos);
+        }
+        return reducir(puntos,epsilon);
+    }
+    private List<Point>
 }
