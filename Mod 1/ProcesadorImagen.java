b3761757c9c638f4d56ec0702c8a7d37eabfd02e
Daniel <danielnavarrom@ciencias.unam.mx>
2026-09-26 00:19:53 -0600

    Fix ya estan todos los metodos borrados

--- Mod 1/ProcesadorImagen.java
+++ Mod 1/ProcesadorImagen.java
@@ -31,10 +31,6 @@ public class ProcesadorImagen {
     }
 
     /**
-     * Ve haceindo los que quieras, yo hare el de detectar color y separar fondo.
-     */
-
-    /**
      * Yo estoy haciendo este
      */
     private void cargarImagen(String rutaArchivo) throws Exception {
@@ -42,18 +38,105 @@ public class ProcesadorImagen {
         imagen = ImageIO.read(archivo);
 
         int anchoImagen = imagen.getWidth();
-        int alturaImagem = imagen.getHeight();
+        int alturaImagen = imagen.getHeight();
 
         visitados = new boolean[anchoImagen][alturaImagem];
-        colorFondo = this.imagen.getRGB(0, 0);
+        colorFondo = detectaColorFondo();
     }
 
-    private int detectaColorFondo() {
+    /**
+     * Como se penso:
+     * El problema inicial es que como las figuras se pueden dispiner sobre la imagen de forma arbitraria,
+     * como sabriamos que no van a tocar una esquina de la imagen (como (0,0)), entonces primero se penso 
+     * en comparar las 4 esquinas, pero que pasaria si una figura toca 2 esquinas ? Pues la ultima solucion
+     * fue recorrer todo el perimetro y con ayuda de una tabla hash identificar el color que sale mas veces.
+     */
+    private int detectaColorFondo(){
+        Map<Integer, Integer> colores = new HashMap<>();
+        int ancho = imagen.getWidth();
+        int largo = imagen.getHeight();
 
+        // Se recorre el perimetro completo de la imagen con los 4 fors.
+        // Pasa rgb como key y como valor el numero de veces que aparece key mas 1.
+        for (int x = 0; x < ancho; x++){
+            int rgb = imagen.getRGB(x,0);
+            colores.put(rgb, colores.getOrDefault(rgb, 0) + 1);
+        }
+        
+        for (int y = 1; y < largo; y++){
+            int rgb = imagen.getRGB(0, y);
+            colores.put(rgb, colores.getOrDefault(rgb, 0) + 1);
+        }
+
+        for (int x = 1; x < ancho; x++){
+            int rgb = imagen.getRGB(x, largo - 1);
+            colores.put(rgb, colores.getOrDefault(rgb, 0) + 1);
+        }
+
+        for (int y = 1; y < largo; y++){
+            int rgb = imagen.getRGB(ancho - 1, y);
+            colores.put(rgb, colores.getOrDefault(rgb, 0) + 1);
+        }
+
+        // hace un stream y compara al mas grande por su value, los guarda en maximo.
+        int colorMasFrecuente = 0;
+        Optional<Map.Entry<Integer, Integer>> maximo = colores.entrySet().stream().max(Map.Entry.comparingByValue());
+        if (maximo.isPresent()){
+            colorMasFrecuente = maximo.get().getKey();
+        }
+        return colorMasFrecuente;
     }
 
+    /**
+     * Como se penso: 
+     * Se tenia que separar el fondo de las figuras usando "Flood fill" que basicamente
+     * toma una coordenada y a partir de ahi empieza a recorrer a sus vecinos de forma que
+     * esquiva las figuraa, esto se ve reflejado en el arreglo "visitados" que se marca con
+     * true cada que encuentre fondo. Para la busqueda se ocupo una cola que ira almacenando las 
+     * coordenadas de los pixeles que sean fondo, termina el ciclo hasya que la cola este vacia.
+     * Al inicio se recorre todo el perimetro de la imagen para evitar el caso donde una o mas 
+     * figuras cortan la imagen en 2.
+     */
     private void separarFondoBFS() {
+        Queue<Point> cola = new LinkedList<>();
+        int colorF = detectaColorFondo();
+        int ancho = imagen.getWidth();
+        int largo = imagen.getHeight();
 
+        visitados = new boolean[largo][ancho];
+
+        for (int x = 0; x < ancho; x++){
+            int rgb = imagen.getRGB(x,0); 
+            if (colorF == rgb){
+                cola.add(new Point(x,0));
+                visitados[x][0] = true;
+            }
+        }
+
+        for (int y = 1; y < largo; y++){
+            int rgb = imagen.getRGB(0, y); 
+            if (colorF == rgb){
+                cola.add(new Point(y,0));
+                visitados[0][y] = true;
+            }
+        }
+
+        for (int x = 1; x < ancho; x++){
+            int rgb = imagen.getRGB(x, largo - 1); 
+            if (colorF == rgb){
+                cola.add(new Point(x,largo - 1));
+                visitados[x][largo-1] = true;
+            }
+        }
+
+        for (int y = 1; y < largo; y++){
+            int rgb = imagen.getRGB(ancho - 1, y); 
+            if (colorF == rgb){
+                cola.add(new Point(ancho - 1, y));
+                visitados[ancho-1][y] = true;
+            }
+        }
+        // Empieza el cilco while, pero ya a mimir xd 
     }
 
     /**
